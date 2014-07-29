3befbd5fe17c39397c4a055f0f8f88a7f3503469
huyle333 <huyle333@yahoo.com>
2014-07-29 08:35:11 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width=device-width, initial-scale = 1">
+		<link href = "css/bootstrap.min.css" rel="stylesheet">
+		<link href = "css/style.css" rel="stylesheet">
+		<link rel= "shortcut icon" href="img/favicon.ico">
+		<link href= "http://fonts.googleapis.com/css?family=Ubuntu|Open+Sans:400,600" rel="stylesheet" />
+	</head>
+	<title>
+		TimeConnect - The end of manual time cards
+	</title>
+	<body data-spy="scroll" data-target="#navigation" id="home">
+		<div class="navbar-wrapper">
+	      <div class="container">
+	        <div class="navbar navbar-default navbar-static-top" role="navigation">
+	          <div class="container">
+	            <div class="navbar-header">
+	              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	              </button>
+	              <a href="#"><img src = "img/timeclock_logo_transparent.png" height = "30px" width = "206px" style= "margin-top: 12px"></a>
+	            </div>
+	            <div class="navbar-collapse collapse">
+	              <ul class="nav navbar-nav navbar-right">
+	                <li><a href="#">Small Businesses</a></li>
+	                <li><a href="#about">Accountants and Bookkeepers</a></li>
+	                <li class="dropdown">
+	                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">More <span class="caret"></span></a>
+	                  <ul class="dropdown-menu" role="menu">
+	                    <li><a href="#">Features</a></li>
+	                    <li><a href="#">Pricing</a></li>
+	                    <li><a href="#">Add-ons</a></li>
+	                    <li class="divider"></li>
+	                    <li><a href="#">Support</a></li>
+	                    <li><a href="#">Blog</a></li>
+	                  </ul>
+	                </li>
+	                <li class="active"><a href="#login">Log-in</a></li>
+	              </ul>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+
+	    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+	      <div class="carousel-inner">
+	        <div class="item active">
+	          <img src="img/AssPNa3.jpg" alt="TimeConnect">
+	          <div class="container">
+	            <div class="carousel-caption">
+	              <h1 style="background-color:rgba(75,75,75,0.7); padding: 10px; border-radius: 10px">The end of manual time cards.</h1>
+	              <br>
+	              <p><a class="btn btn-lg btn-primary" href="#" role="button">2-Minute Overview</a> &nbsp; <a class="btn btn-lg btn-primary" href="#" role="button">30-Day Free Trial</a></p>
+	              <br>
+	              <p>See why others choose TimeConnect</p>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+
+		<div class = "container marketing" style ="margin-top:-50px">
+			<h1 style="text-align:center">TimeConnect at a glance</h1>
+			<br>
+			<p style="text-align:center; color: #ADADAD">Check-in or out from work without any hassle. No more paper time cards. <br>It's an automated time card system that's simple, smart, and accurate.</p>
+			<br>
+		</div>
+
+		<div class="jumbotron" style="background-color: #44aefc">
+      		<div class="container text-center">
+      			<h2 style="color: white"></h2>
+      		</div>
+    	</div>
+
+    	<div class="jumbotron" style="background-color: #1eb5ea; margin-top: -30px">
+
+      		<div class="container text-center">
+      			<img style="margin: auto" class = "thumbnail" src = "img/timeclock_logo_white_small.png" height ="100px">
+      			</br>
+      			<h3 style="color: white">Innovative startup that prizes customer satisfaction</h2>
+      		</div>
+    	</div>
+
+    	<div class="jumbotron" style="background-color: white; margin-top: -30px">
+      		<div class="container text-center">
+      			<h2>Popular features - Why you'll love TimeConnect.</h2>
+      			<br>
+      			<p style="text-align:center; color: #ADADAD">Automated software and hardware that allows you to save time and money </br>used on correcting employee time entries.</p>
+      		</div>
+    	</div>
+
+		<div class="container marketing">
+    		<div class = "text-center jumbotron well" id = "end">
+    		<h2>30-Day Trial</h2>	
+        </br>
+		<form class = "form-inline" name="contactform" role="form" method ="post" action = "send_form_email.php">
+		  <div class="form-group">
+		    <label for="contact-name" class="col-lg-2 control-label">Name: </label>
+		    <input type="text" class="form-control" id="contact-name" name="contact-name" placeholder="Enter Full Name" maxlength="50" size="30">
+		  </div>
+		  </br>
+		  </br>
+		  <div class="form-group">
+		    <label for="email" class="col-lg-2 control-label">Email: </label>
+		    <input type="email" class="form-control" id="email" name="email" placeholder="Enter your Email" maxlength="80" size="30">
+		  </div>
+		  </br>
+		  </br>
+		  <div class="form-group">
+		    <label for="contact-sub" class="col-lg-2 control-label">Subject: </label>
+		    <input type="text" class="form-control" id="contact-sub" name="contact-sub" placeholder="Enter the Subject" maxlength="50" size="30">
+		  </div>
+		  </br>
+		  </br>
+		  <div class="form-group">
+		    <label for="contact-msg" class ="col-lg-2 control-label">Message: </label>
+		    <div class = "col-lg-10">
+		      <textarea class="form-control" id="contact-msg" rows="8" name="contact-msg" placeholder="Enter your Message" maxlength = "1000"></textarea>
+		    </div>
+		  </div>
+		  </br>
+		  </br>
+		  <button type="submit" value = "Submit" class="btn btn-primary">Submit</button>
+		</form>
+		</div>
+		</div>
+
+		</br>
+		</br>
+			
+		<div id = "end" class = "navbar navbar-default" style="height: 250px">
+            <div class = "container">
+            	<div class="row">
+        			<div class="col-lg-4">
+        				<h3>Learn about TimeConnect</h3>
+        			</div>
+        			<div class="col-lg-4">
+        				<h3>Partner with us</h3>
+        			</div>
+        			<div class="col-lg-4">
+        				<h3>Contact & info</h3>
+        			</div>
+        		</div>
+            	<a href = "#" class = "navbar-btn btn-primary btn pull-right">Back to top</a>
+                <p class = "navbar-text pull-left">&copy; 2014 - TimeConnect. All rights reserved.</p>
+            </div>
+      	</div>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+    
+	    <script>
+	      $("#navigation ul li a[href^='#']").on('click', function(e) {
+	        // prevent default anchor click behavior
+	        e.preventDefault();
+	        // store hash
+	        var hash = this.hash;
+	        // animate
+	        $('html, body').animate({
+	          scrollTop: $(this.hash).offset().top
+	        }, 300, function(){
+	          // when done, add hash to url
+	          // (default click behaviour)
+	          window.location.hash = hash;
+	        });
+	      });
+	    </script>
+</html>
